45b3baa18adb396e2e4ea6d41e3ca2465fc7b156
HackerA777 <tyapkov.artem@gmail.com>
2022-11-22 23:17:45 +0400

    optimized code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from time import perf_counter
-from tables import T2, Pi, T
+from tables import T2, Pi
 import numba
 from numba import cuda
 
@@ -17,13 +17,11 @@ def print_size(size):
 
 
 def iter_keys(keys):
-    # keys = np.fromstring(keys, np.uint8)
     round_keys = [keys[4*i:i*4+4] for i in range(8)]
     return round_keys
 
 
 @cuda.jit(numba.void(numba.uint8[:], numba.uint8[:], numba.uint8[:]), device=True)
-# @numba.njit(numba.void(numba.uint8[:], numba.uint8[:], numba.uint8[:]), nogil=True, fastmath=True)
 def xor(a: np.uint8, b: np.uint8, c: np.uint8):
     a = a.view(np.uint32)
     b = b.view(np.uint32)
@@ -32,7 +30,6 @@ def xor(a: np.uint8, b: np.uint8, c: np.uint8):
 
 
 @cuda.jit(numba.void(numba.uint8[:], numba.uint8[:], numba.uint8[:]), device=True)
-# @numba.njit(numba.void(numba.uint8[:], numba.uint8[:], numba.uint8[:]), nogil=True, fastmath=True)
 def add_32(a: np.uint8, b: np.uint8, c: np.uint8):
     a = a.view(np.uint32)
     b = b.view(np.uint32)
@@ -41,8 +38,7 @@ def add_32(a: np.uint8, b: np.uint8, c: np.uint8):
 
 
 @cuda.jit(numba.void(numba.uint8[:], numba.uint8[:], numba.uint8[:, :]), device=True)
-# @numba.njit(numba.void(numba.uint8[:], numba.uint8[:]), nogil=True)
-def magma_T(in_data: np.uint8, out_data: np.uint8, table: np.array):
+def magma_T(in_data: np.uint8, out_data: np.uint8, table: np.ndarray):
     out_data[0] = T2(0, in_data[0], table)
     out_data[1] = T2(1, in_data[1], table)
     out_data[2] = T2(2, in_data[2], table)
@@ -50,7 +46,6 @@ def magma_T(in_data: np.uint8, out_data: np.uint8, table: np.array):
 
 
 @cuda.jit(numba.void(numba.uint8[:], numba.uint8[:], numba.uint8[:], numba.uint8[:, :]), device=True)
-# @numba.njit(numba.void(numba.uint8[:], numba.uint8[:], numba.uint8[:]), nogil=True)
 def magma_g(round_key, block, internal, table):
     add_32(block, round_key, internal)
     magma_T(internal, internal, table)
@@ -59,57 +54,40 @@ def magma_g(round_key, block, internal, table):
 
 
 @cuda.jit(numba.void(numba.uint8[:], numba.uint8[:], numba.uint8[:], numba.uint8[:], numba.uint8[:, :]), device=True)
-# @numba.njit(numba.void(numba.uint8[:], numba.uint8[:], numba.uint8[:], numba.uint8[:]), nogil=True)
 def magma_G(round_key, block, out_block, t, table):
     l = block[:4]
     r = block[4:]
     for i in range(4):
         t[i] = r[i]
-    # t[:4] = r[:4]
-    # t = r
     magma_g(round_key, l, t, table)
     xor(r, t, t)
     for i in range(4):
-        t[i] = l[i]
-    # r[0:4] = l[:4]
-    # r = l
+        r[i] = l[i]
     for i in range(4):
         l[i] = t[i]
-    # l[...] = t
-    # l = t
     for i in range(4):
         out_block[i] = l[i]
         out_block[i+4] = r[i]
-    # out_block[:4] = l
-    # out_block[4:] = r
 
 
 @cuda.jit(numba.void(numba.uint8[:], numba.uint8[:], numba.uint8[:], numba.uint8[:], numba.uint8[:, :]), device=True)
-# @numba.njit(numba.void(numba.uint8[:], numba.uint8[:], numba.uint8[:], numba.uint8[:]), nogil=True)
 def magma_G_Last(round_key, block, out_block, t, table):
     l = block[:4]
     r = block[4:]
     for i in range(4):
         t[i] = r[i]
-    # t[...] = r
-    # t = r
     magma_g(round_key, l, t, table)
     xor(r, t, t)
     for i in range(4):
         r[i] = t[i]
-    # r[...] = t
-    # r = t
     for i in range(4):
         out_block[i] = l[i]
         out_block[i+4] = r[i]
-    # out_block[:4] = l
-    # out_block[4:] = r
 
 
 @cuda.jit(numba.void(numba.uint8[:, :], numba.uint8[:], numba.uint8[:], numba.uint8[:, :]), device=True)
-# @numba.njit(numba.void(numba.uint8[:, :], numba.uint8[:], numba.uint8[:]), nogil=True)
 def encrypt(round_keys: np.array, block: np.uint8, out_block: np.uint8, table: np.array):
-    t = cuda.shared.array(shape=8, dtype=numba.uint8)
+    t = cuda.local.array(8, numba.uint8)
     magma_G(round_keys[0], block, out_block, t, table)
     for i in range(1, 31):
         magma_G(round_keys[i], out_block, out_block, t, table)
@@ -117,10 +95,8 @@ def encrypt(round_keys: np.array, block: np.uint8, out_block: np.uint8, table: n
 
 
 @cuda.jit(numba.void(numba.uint8[:, :], numba.uint8[:], numba.uint8[:], numba.uint8[:, :]), device=True)
-# @numba.njit(numba.void(numba.uint8[:, :], numba.uint8[:], numba.uint8[:]), nogil=True)
 def decrypt(round_keys: np.array, block: np.uint8, out_block: np.uint8, table: np.array):
-    # t = np.empty(4, np.uint8)
-    t = cuda.shared.array(shape=8, dtype=numba.uint8)
+    t = cuda.local.array(8, numba.uint8)
     magma_G(round_keys[31], block, out_block, t, table)
     for i in range(30, 0, -1):
         magma_G(round_keys[i], out_block, out_block, t, table)
@@ -137,23 +113,13 @@ def check_len_main_block(block: str):
 
 
 @cuda.jit()
-# @numba.njit(numba.void(numba.uint8[:, :], numba.uint8[:], numba.uint8[:]), parallel=True, nogil=True)
 def main_encrypt(encryption_keys, block, encrypt_block, table):
-    # for i in numba.prange(0, len(block)//8):
-    #     encrypt(encryption_keys, block[i*8:i*8 + 8], encrypt_block[i*8:i*8 + 8])
-    # t = block[:8].copy()
-    # t = cuda.shared.array(shape=8, dtype=numba.uint8)
     for i in range(8*cuda.grid(1), len(block), 8*cuda.gridsize(1)):
         encrypt(encryption_keys, block[i:i+8], encrypt_block[i:i+8], table)
 
 
 @cuda.jit()
-# @numba.njit(numba.void(numba.uint8[:, :], numba.uint8[:], numba.uint8[:]), parallel=True, nogil=True)
 def main_decrypt(encryption_keys, encrypt_block, decrypt_block, table):
-    # for i in numba.prange(0, len(encrypt_block)//8):
-    #     decrypt(encryption_keys, encrypt_block[i*8:i*8+8], decrypt_block[i*8:i*8+8])
-    # t = encrypt_block[:8].copy()
-    # t = cuda.shared.array(shape=8, dtype=numba.uint8)
     for i in range(8*cuda.grid(1), len(encrypt_block), 8*cuda.gridsize(1)):
         decrypt(encryption_keys, encrypt_block[i:i+8], decrypt_block[i:i+8], table)
 
@@ -162,15 +128,14 @@ def main():
     # key = ('1'*4+'2'*4+'3'*4+'4'*4)*2
     key = np.array([204, 221, 238, 255, 136, 153, 170, 187, 68,  85, 102, 119, 0, 17, 34, 51, 243, 242, 241, 240, 247,
                     246, 245, 244, 251, 250, 249, 248, 255, 254, 253, 252], dtype=np.uint8)
-    # main_block = np.random.randint(0, 250, size=1048576, dtype=np.uint8)
+    main_block = np.random.randint(0, 250, size=1073741824, dtype=np.uint8)
     round_keys = iter_keys(key)
     encryption_keys = round_keys * 3 + round_keys[::-1]
     encryption_keys = np.array(encryption_keys, dtype=np.uint8)
-    # decryption_keys = encryption_keys[::-1]
     # block = check_len_main_block(main_block)
-    # block = main_block
-    block = np.array([16, 50, 84, 118, 152, 186, 220, 254] * 81920, dtype=np.uint8)
-    main_block = block
+    block = main_block
+    # block = np.array([16, 50, 84, 118, 152, 186, 220, 254] * 1310720, dtype=np.uint8)
+    # main_block = block
     print_size(block.size)
     encrypt_block = block
     decrypt_block = block
@@ -184,17 +149,16 @@ def main():
     print("-"*20)
     print("Start encrypt")
     start = perf_counter()
-    main_encrypt[128, 100](encryption_keys, block, encrypt_block_cuda, P_table)
+    main_encrypt[128, 128](encryption_keys, block, encrypt_block_cuda, P_table)
     cuda.synchronize()
     end = perf_counter()
-    # main_encrypt.parallel_diagnostics(level=4)
     encrypt_block = encrypt_block_cuda.copy_to_host()
     print(f'Encrypt_block: {encrypt_block[:8]}')
     # зашифрованный текст в приложении ГОСТа: [78, 233, 1, 229, 194, 216, 202, 61]
     print(f'Time encrypt: {end - start} sec')
     print("Start decrypt")
     start = perf_counter()
-    main_decrypt[128, 100](encryption_keys, encrypt_block_cuda, decrypt_block_cuda, P_table)
+    main_decrypt[128, 128](encryption_keys, encrypt_block_cuda, decrypt_block_cuda, P_table)
     cuda.synchronize()
     print(f'Time decrypt: {perf_counter() - start} sec')
     decrypt_block = decrypt_block_cuda.copy_to_host()
--- tables.py
+++ tables.py
@@ -3,17 +3,6 @@ import numba
 from numba import cuda
 
 
-# Pi = np.array([
-#         [1, 7, 14, 13, 0, 5, 8, 3, 4, 15, 10, 6, 9, 12, 11, 2],
-#         [8, 14, 2, 5, 6, 9, 1, 12, 15, 4, 11, 0, 13, 10, 3, 7],
-#         [5, 13, 15, 6, 9, 2, 12, 10, 11, 7, 8, 1, 4, 3, 14, 0],
-#         [7, 15, 5, 10, 8, 1, 6, 13, 0, 9, 3, 14, 11, 4, 2, 12],
-#         [12, 8, 2, 1, 13, 4, 15, 6, 7, 0, 10, 5, 3, 14, 9, 11],
-#         [11, 3, 5, 8, 2, 15, 10, 13, 14, 1, 7, 4, 12, 9, 6, 0],
-#         [6, 8, 2, 3, 9, 10, 5, 12, 1, 14, 4, 7, 11, 13, 0, 15],
-#         [12, 4, 6, 2, 10, 5, 11, 9, 14, 8, 13, 7, 0, 3, 15, 1]
-#     ], np.uint8)
-
 Pi = np.array([
     [12, 4, 6, 2, 10, 5, 11, 9, 14, 8, 13, 7, 0, 3, 15, 1],
     [6, 8, 2, 3, 9, 10, 5, 12, 1, 14, 4, 7, 11, 13, 0, 15],
@@ -26,18 +15,8 @@ Pi = np.array([
 ], np.uint8)
 
 
-# @cuda.jit(device=True)
-@numba.njit(numba.uint8(numba.int32, numba.uint8), nogil=True, fastmath=True)
-def T(i: np.int32, val: np.uint8):
-    l = val & 0x0f
-    h = (val & 0xf0) >> 4
-    l = Pi[i*2, l]
-    h = Pi[i*2+1, h]
-    return (h << 4) | l
-
-
 @cuda.jit(numba.uint8(numba.int32, numba.uint8, numba.uint8[:, :]), device=True)
-def T2(i: np.uint32, val: np.uint8, P_table: np.array):
+def T2(i: np.int32, val: np.uint8, P_table: np.ndarray):
     l = val & 0x0f
     h = (val & 0xf0) >> 4
     l = P_table[i * 2, l]
